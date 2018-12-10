42796307c9091891b24bdfe849bec57a55fcb9c1
Anav <janbul6@gmail.com>
2018-12-10 21:47:18 +0100

    Changed DI from ToConstant<> to To<>

--- Nuntium/App.xaml.cs
+++ Nuntium/App.xaml.cs
@@ -19,31 +19,38 @@ namespace Nuntium
             Current.MainWindow = new MainWindow();
 
             //TODO: this is just a quick hack for fixing the issue width not binding TextEditor's editor at startup
-            IoC.Kernel.Get<TextEditorViewModel>().SelectedMenu = MenuCategories.Options;
-            IoC.Kernel.Get<TextEditorViewModel>().SelectedMenu = MenuCategories.Format;
+            //IoC.Kernel.Get<TextEditorViewModel>().SelectedMenu = MenuCategories.Options;
+            //IoC.Kernel.Get<TextEditorViewModel>().SelectedMenu = MenuCategories.Format;
 
             Current.MainWindow.Show();
         }
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            IoC.Kernel.Dispose();
+            base.OnExit(e);
+        }
+
         private void SetupIoC()
         {
-            IoC.Kernel.Bind<IEmailService>().ToConstant(new MockedEmailService());
+            IoC.Kernel.Bind<IEmailService>().To<MockedEmailService>();
 
-            IoC.Kernel.Bind<IEventAggregator>().ToConstant(new EventAggregator());
+            IoC.Kernel.Bind<IEventAggregator>().To<EventAggregator>();
+
+            IoC.Kernel.Bind<AddressSectionViewModel>().To<AddressSectionViewModel>();
+
+            IoC.Kernel.Bind<AttachmentsSectionViewModel>().To<AttachmentsSectionViewModel>();
+
+            IoC.Kernel.Bind<SearchSectionViewModel>().To<SearchSectionViewModel>();
+
+            IoC.Kernel.Bind<TextEditorViewModel>().To<TextEditorViewModel>();
+
+            IoC.Kernel.Bind<FormattingSubmenuViewModel>().ToConstant(new FormattingSubmenuViewModel());
 
             IoC.Kernel.Bind<ApplicationViewModel>().ToConstant(new ApplicationViewModel());
 
             IoC.Kernel.Bind<SideMenuViewModel>().ToConstant(new SideMenuViewModel());
 
-            IoC.Kernel.Bind<AddressSectionViewModel>().ToConstant(new AddressSectionViewModel());
-
-            IoC.Kernel.Bind<AttachmentsSectionViewModel>().ToConstant(new AttachmentsSectionViewModel());
-
-            IoC.Kernel.Bind<SearchSectionViewModel>().ToConstant(new SearchSectionViewModel());
-
-            IoC.Kernel.Bind<FormattingSubmenuViewModel>().ToConstant(new FormattingSubmenuViewModel());
-
-            IoC.Kernel.Bind<TextEditorViewModel>().ToConstant(new TextEditorViewModel());
         }
     }
 }
